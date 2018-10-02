36c7be661812bf34b6926387c77989e7931a4a80
malvi <mrp92@njit.edu>
2018-10-02 13:52:25 -0400

    fix: edit code

--- Public/index.php
+++ Public/index.php
@@ -3,7 +3,34 @@ main:: start();
 
 class main {
     static public function start() {
-        $records =csv::getRecords();
-        echo 'test';
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system:: printPage($table);
     }
 }
+ class csv
+ {
+     static public function getRecords()
+     {
+         echo 'test';
+     }
+ }
+
+
+ class html{
+
+    static public function generateTable($records)
+    {
+        $table = $records;
+        return $table;
+    }
+
+ }
+ class system{
+    
+    static public function printPage($page)
+    {
+        echo $page;
+    }
+
+ }
