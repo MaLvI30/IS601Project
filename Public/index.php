f1b28583b609089da7bd67acd0e83c322fb40d7b
malvi <mrp92@njit.edu>
2018-10-06 17:02:42 -0400

    Features: create a function called create property.

--- Public/index.php
+++ Public/index.php
@@ -9,16 +9,52 @@ class main
 class csv{
     static public function getRecords(){
 
+        $file = fopen($filename,"r");
+        $fields = array();
+        $count = 0;
+
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if ($count == 0) {
+                $fields = $record;
+            }
+            else
+            {
+                $records[] = RecordFactory::create($fields, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+
     }
 }
 class record{
-    public function __construct()
+    public function __construct(Array $fields = null, Array $values = null)
     {
+        $record = array_combine($fields,$values);
+
+        foreach ($record as $property => $value)
+        {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function createProperty($property,$value)
+    {
+        $this->{$property} = $value;
+        $property = '<th>' .$property.'</th>';
+        $value = '<td>' .$value.'</td>';
     }
 }
 class recordFactory{
-    public static function create(){
-        
+    public static function create(Array $fields = null,Array $values = null){
+        {
+            $record = new record($fields,$values);
+
+            return $record;
+        }
+
     }
 }
 class html{
