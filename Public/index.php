e9611b13db8cc84d80b82c36792ec18be1dd197c
malvi <mrp92@njit.edu>
2018-10-06 17:12:19 -0400

    Task: Added html table

--- Public/index.php
+++ Public/index.php
@@ -58,9 +58,33 @@ class recordFactory{
     }
 }
 class html{
-    static public function generateTable(){
+    static public function createTable($array){
+
+        //table row
+        $html = '<table class="table table-striped">';
+        // header row
+        $html .= '<tr>';
+        foreach($array[0] as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+        $html .= '</tr>';
+
+        // data rows
+        foreach( $array as $key=>$value){
+            $html .= '<tr>';
+            foreach($value as $key2=>$value2){
+                $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+            }
+            $html .= '</tr>';
+        }
+
+        // finish table and return it
+
+        $html .= '</table>';
+        return $html;
 
     }
+    
 }
 class system{
     static public function printPage(){
